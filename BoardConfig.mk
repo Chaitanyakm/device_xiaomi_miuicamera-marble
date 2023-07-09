8559d0e59984ef4e4bf1c75ea1acdfc0111c3e0a
Arian <arian.kulmer@web.de>
2023-07-09 23:17:33 +0200

    Initial MiuiCamera for zeus

---
+++ BoardConfig.mk
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from the proprietary version
+include vendor/xiaomi/miuicamera-zeus/BoardConfigVendor.mk
+
+MIUICAMERA_PATH := device/xiaomi/miuicamera-zeus
+
+# Properties
+TARGET_SYSTEM_PROP += $(MIUICAMERA_PATH)/system.prop
+
+# Sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(MIUICAMERA_PATH)/sepolicy/vendor
---
+++ device.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from the proprietary version
+$(call inherit-product, vendor/xiaomi/miuicamera-zeus/miuicamera-zeus-vendor.mk)
+
+# Priv-app permission
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-miuicamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-miuicamera.xml
+
+# Shims
+PRODUCT_PACKAGES += \
+    libgui_shim_miuicamera
+
+# Sysconfig
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/miuicamera-hiddenapi-package-allowlist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/miuicamera-hiddenapi-package-allowlist.xml
