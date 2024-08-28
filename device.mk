914ea62d0e30bea73f37eb0de7706ab59eb7b892
Arian <arian.kulmer@web.de>
2024-08-28 14:45:03 +0200

    Drop explicit building of libgui shim
    Change-Id: I7bbc4e7b4ba12fe427ce2d986125fca8f24731f2

--- device.mk
+++ device.mk
@@ -15,10 +15,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-miuicamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-miuicamera.xml
 
-# Shims
-PRODUCT_PACKAGES += \
-    libgui_shim_miuicamera
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
