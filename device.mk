81105e2fe9987254d4c16a8ed1892af9a9fa64f7
timur_23_1337 <mullaiarov2002@gmail.com>
2026-04-10 14:16:21 +0500

    Add new adaptive Leica camera icon

--- device.mk
+++ device.mk
@@ -7,6 +7,10 @@
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/miuicamera-marble/miuicamera-marble-vendor.mk)
 
+# Overlays
+PRODUCT_PACKAGES += \
+    MiuiCameraOverlay
+
 # Public libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries-xiaomi.txt:$(TARGET_COPY_OUT_SYSTEM)/etc/public.libraries-xiaomi.txt
